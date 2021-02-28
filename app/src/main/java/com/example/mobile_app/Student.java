a2acee1f3ce8bda3e6c9320895226f737d77f20a
trotyluk <trotyluk@users.noreply.github.com>
2021-02-28 00:04:32 +0000

    classes for Student and Books created

---
+++ app/src/main/java/com/example/mobile_app/Student.java
@@ -0,0 +1,57 @@
+package com.example.mobile_app;
+
+public class Student {
+    // class to create Student object
+    //Variables
+    private String firstName, lastName, email, password;
+
+    //Constructor to make and write object to FireBase
+
+
+    public Student(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    // Constructor to make object and read data into object from FireBase
+
+    public Student() {
+    }
+    //Methods
+    //get
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    // set
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
