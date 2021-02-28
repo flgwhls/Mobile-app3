f8f4e23c9ae8b5d638b1ef59e5c3b32faaeb21cc
trotyluk <trotyluk@users.noreply.github.com>
2021-02-28 18:16:27 +0000

    book page started

--- app/src/main/java/com/example/mobile_app/Register.java
+++ app/src/main/java/com/example/mobile_app/Register.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Register extends AppCompatActivity {
+  // Variables
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
     }
 }
