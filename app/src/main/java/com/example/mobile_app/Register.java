4b5567b325bf93fd329ab8c60670db16751d4f83
trotyluk <trotyluk@users.noreply.github.com>
2021-03-01 00:48:39 +0000

    book page launcher in Register Page created

--- app/src/main/java/com/example/mobile_app/Register.java
+++ app/src/main/java/com/example/mobile_app/Register.java
@@ -13,12 +13,14 @@ public class Register extends AppCompatActivity {
     Button register;
     // for testing bookpage
     Button bookpage;
+    Button bookreg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         bookpage = findViewById(R.id.btn_bookpage); // delete when not needed
+        bookreg = findViewById(R.id.btn_reg_registerbook); //delete when not needed
 
         //-----------------------------------------------------------------------
         // temporary for test bookpage
@@ -30,9 +32,20 @@ public class Register extends AppCompatActivity {
                 startActivity(i);
             }
             //------------  END TEMPORARY bookPage test --------------------------
-            // delete when not needed
+
 
         });
+        //------------ temporary for register books in firebase
 
+        // delete when not needed
+        bookreg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Register.this,BookRegister.class);
+
+                startActivity(i);
+            }
+        });
+        // ------- END of temporary book register
     }
 }
