1425f7ca9c731c59e437d28e9081785c86801b9e
ivanuzzu94 <79015900+ivanuzzu94@users.noreply.github.com>
2021-03-02 23:09:41 +0100

    update

--- app/src/main/java/com/example/mobile_app/Login.java
+++ app/src/main/java/com/example/mobile_app/Login.java
@@ -3,12 +3,21 @@ package com.example.mobile_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
+
+    EditText email,password;
+    Button log;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email=findViewById(R.id.et_log_login);
+        password=findViewById(R.id.et_log_pasword);
+        log=findViewById(R.id.btn_log_login);
     }
 }
