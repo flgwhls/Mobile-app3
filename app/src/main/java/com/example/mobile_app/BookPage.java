9c6edf1c1c7540284b4c2859fbc3d83e6437a9ea
trotyluk <trotyluk@users.noreply.github.com>
2021-03-02 23:46:40 +0000

    BookPage layout finished

--- app/src/main/java/com/example/mobile_app/BookPage.java
+++ app/src/main/java/com/example/mobile_app/BookPage.java
@@ -4,12 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class BookPage extends AppCompatActivity {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
 
+import java.util.ArrayList;
+
+public class BookPage extends AppCompatActivity {
+    // Variable declaration
+
+    ArrayList booklist = new ArrayList<>();
+    DatabaseReference dbref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_page);
+        // Activity to View book form database
 
 
     }
