6f861844a2695e1fee81e24fe3af0917184f5b1b
ivanuzzu94 <79015900+ivanuzzu94@users.noreply.github.com>
2021-03-04 23:32:11 +0100

    registration and login pages are done!

--- app/src/main/java/com/example/mobile_app/Dashboard.java
+++ app/src/main/java/com/example/mobile_app/Dashboard.java
@@ -2,13 +2,34 @@ package com.example.mobile_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Dashboard extends AppCompatActivity {
+
+    Button btn_Logout;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        btn_Logout=findViewById(R.id.btn_logout);
+
+        btn_Logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent IntoWelcomePage= new Intent(Dashboard.this,WelcomePage.class);
+                startActivity(IntoWelcomePage);
+            }
+        });
     }
 }
